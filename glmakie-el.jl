2d2de7c3921fba881e9466a880ae5f7a15f17679
mochar <mocharrout@gmail.com>
2026-08-18 00:12:40 +0200

    tcp server

--- glmakie-el.jl
+++ glmakie-el.jl
@@ -3,18 +3,67 @@ using FixedPointNumbers: N0f8
 
 const SHM_PATH = "/dev/shm/glmakie_emacs.bin"
 
-f = Figure()
-lines(f[1,1], sin.(1:100))
-screen = f.scene.current_screens[1]
-framebuf = screen.framebuffer.buffers[:color]
-WIDTH, HEIGHT = size(framebuf)
+# f = Figure()
+# lines(f[1,1], sin.(1:100))
+# display(f)
+# screen = f.scene.current_screens[1]
+# framebuf = screen.framebuffer.buffers[:color]
+# WIDTH, HEIGHT = size(framebuf)
 
-io = open(SHM_PATH, "w+")
-truncate(io, WIDTH * HEIGHT * 4) # 4 bytes per pixel (RGBA)
+# io = open(SHM_PATH, "w+")
+# truncate(io, WIDTH * HEIGHT * 4) # 4 bytes per pixel (RGBA)
 
-shared = mmap(io, Matrix{RGBA{N0f8}}, (HEIGHT, WIDTH))
+# shared = mmap(io, Matrix{RGBA{N0f8}}, (HEIGHT, WIDTH))
 
 
-cpu_buffer = colorbuffer(screen)
-copyto!(shared, cpu_buffer')
-Mmap.sync!(shared)
+# cpubuf = colorbuffer(screen)
+# copyto!(shared, cpubuf')
+# Mmap.sync!(shared)
+
+
+### Server
+
+module Server
+
+using Sockets, Base.Threads
+
+global server = listen(8888)
+global conns = []
+
+server_task = @spawn begin
+    while true
+        conn = accept(server)
+        push!(conns, conn)
+        @spawn begin
+            try
+                while isopen(conn)
+                    line = readline(conn)
+
+                    if isempty(line)
+                        println("Connection killed")
+                        deleteat!(conns, findall(x->x==conn, conns))
+                        break
+                    end
+
+                    println("Got $line")
+                    println(conn, "Nice")
+                    
+                    # parts = split(line)
+                    # if parts[1] == "RESIZE"
+                    #     w = parse(Int, parts[2])
+                    #     h = parse(Int, parts[3])
+                    #     resize_and_render(w, h)
+                    #     # Acknowledge completion back to Emacs
+                    #     println(conn, "DONE") 
+                    # end
+                end
+            catch e
+                println("Connection error: ", e)
+            end
+        end
+    end
+end
+
+errormonitor(server_task)
+
+end
--- scratch.el
+++ scratch.el
@@ -1,4 +1,40 @@
 
+;;; Server
+
+(defvar glmakie--process nil)
+(defvar glmakie--process-buf-name "*glmakie process*")
+
+(defun glmakie-connect ()
+  (let ((process-buf (get-buffer-create glmakie--process-buf-name)))
+    (setq glmakie--process
+          (open-network-stream
+           "glmakie-process"
+           process-buf
+           "localhost"
+           8888))
+    (set-process-filter glmakie--process #'glmakie--server-process-filter)
+    ))
+
+(defun glmakie--server-process-filter (proc str)
+  (when (buffer-live-p (process-buffer proc))
+    (with-current-buffer (process-buffer proc)
+      (goto-char (point-max))
+      (insert str)
+      (set-marker (process-mark proc) (point))
+
+      (message "Got: %s" str))))
+
+(defun glmakie--send-resize (height width)
+  (process-send-string
+   glmakie--process
+   (format "RESIZE %d %d\n" height width)))
+
+(glmakie-connect)
+
+
+
+;;; Module
+
 (defun glmakie-reload ()
   (let ((tmp (make-temp-file "glmakie_el_" nil ".so")))
     (copy-file "glmakie_el.so" tmp t)
@@ -10,6 +46,10 @@
 
   )
 
+;;; Canvas
+
+(glmakie--send-resize 100 300)
+    
 ;; The image specification object uniquely (with respect to ‘eq’) identifies a
 ;; canvas image object.
 (setq glmakie-canvas
@@ -17,6 +57,11 @@
               :id glmakie2
               :data-width 712
               :data-height 423))
+
+(defun glmakie-resize (height width)
+  (setf (plist-get (cdr glmakie-canvas) :data-height) height
+        (plist-get (cdr glmakie-canvas) :data-width) width)
+  (glmakie--send-resize height width))
 
 (progn
   (glmakie-update glmakie-canvas)
